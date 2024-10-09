6d9ab6fb43295ff97122b858112ff719135ff266
Victoria Grishkova <grishkova@infosim.net>
2024-10-09 23:08:47 +0200

    remove default placeholders & create header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pc-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+
+}
