8bebdb91c842da053e9e5bcde93b89656086e457
Victoria Grishkova <grishkova@infosim.net>
2024-10-11 23:38:25 +0200

    add responsive layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatMenu, MatMenuTrigger} from '@angular/material/menu';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'pc-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  imports: [
+    MatIcon,
+    MatMenuTrigger,
+    MatMenu,
+  ],
+  animations: [
+    // Define the custom animation trigger
+    trigger('animateText', [
+      // Defining the hidden state
+      state('menu', style({
+        transform: 'rotate(0)'
+      })),
+      // Defining the visible state
+      state('close', style({
+        transform: 'rotate(90deg)'
+      })),
+      transition('menu => close', [
+        animate('200ms ease-out')
+      ]),
+      transition('close => menu', [
+        animate('200ms ease-in')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
-
+  icon: 'menu' | 'close' = 'menu';
 }
